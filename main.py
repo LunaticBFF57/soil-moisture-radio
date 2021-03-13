b001b03af04280288a086e5b564a7dafa29b933e
LunaticBFF57 <78350612+LunaticBFF57@users.noreply.github.com>
2021-03-13 11:04:38 +1100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 radio.set_group(69)
+basic.show_string("Moisture Radio")
+
+def on_received_number(receivedNumber):
+    serial.write_value("Soil Moisture", receivedNumber)
+radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
 radio.setGroup(69)
+basic.showString("Moisture Radio")
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    serial.writeValue("Soil Moisture", receivedNumber)
+})
